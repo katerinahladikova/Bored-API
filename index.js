5f9b61be93b4897f329742fc498128d98d4ed513
katerinahladikova <kat.hladikova@email.cz>
2025-08-19 14:07:25 +0200

    fetch api & filter inputs

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-fetch("https://boredapi.katerinahladikova.cz/random")
-.then(r => r.json())
-.then(data => console.log(data))
+const activity = document.getElementById("activity")
+const getActivity = document.getElementById("get-activity")
+
+
+getActivity.addEventListener("click", function(){
+    fetch("https://boredapi.katerinahladikova.cz/random")
+    .then(r => r.json())
+    .then(data => activity.textContent = data.activity)
+})
 
