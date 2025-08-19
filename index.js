2827ac0d069e2d10e8a3e905891d7d50ec2eb170
katerinahladikova <kat.hladikova@email.cz>
2025-08-19 14:59:12 +0200

    get activities

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const activity = document.getElementById("activity")
 const getActivity = document.getElementById("get-activity")
 const filterBoxes = document.querySelectorAll(`input[type="checkbox"]`)
-const api = `https://boredapi.katerinahladikova.cz/filter`
+const api = `https://boredapi.katerinahladikova.cz/`
 
 
 getActivity.addEventListener("click", async function(){
     const data = []
-    for (let filterBox of filterBoxes){
-        if (filterBox.checked){
-            const result = await fetch(`${api}?type=${filterBox.value}`)
-            const r = await result.json()
-            data.push(...r)
+    if ([...filterBoxes].filter(box => box.checked).length > 0){
+        for (let filterBox of filterBoxes){
+            if (filterBox.checked){
+                const result = await fetch(`${api}filter?type=${filterBox.value}`)
+                const r = await result.json()
+                data.push(...r)
+            }
         }
+    } else {
+        const result = await fetch(`${api}random`)
+        const r = await result.json()
+        data.push(r)
     }
 
-    console.log(data)
+    const getRandom = data[Math.floor(Math.random()*data.length)];
+    activity.textContent = getRandom.activity
 })
 
