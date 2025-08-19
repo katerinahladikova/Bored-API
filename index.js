4de998ca59a0beca1ed1d579ad74e6252014c1ec
katerinahladikova <kat.hladikova@email.cz>
2025-08-19 14:47:08 +0200

    filters

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const activity = document.getElementById("activity")
 const getActivity = document.getElementById("get-activity")
+const filterBoxes = document.querySelectorAll(`input[type="checkbox"]`)
+const api = `https://boredapi.katerinahladikova.cz/filter`
 
 
-getActivity.addEventListener("click", function(){
-    fetch("https://boredapi.katerinahladikova.cz/random")
-    .then(r => r.json())
-    .then(data => activity.textContent = data.activity)
+getActivity.addEventListener("click", async function(){
+    const data = []
+    for (let filterBox of filterBoxes){
+        if (filterBox.checked){
+            const result = await fetch(`${api}?type=${filterBox.value}`)
+            const r = await result.json()
+            data.push(...r)
+        }
+    }
+
+    console.log(data)
 })
 
